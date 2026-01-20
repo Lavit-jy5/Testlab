e28c605465982694b6cb4874ae4c52e024fbdb7f
liu <wjy>
2026-01-20 16:07:26 +0800

    INitial conmit

--- src/main/java/com/atguigu/Calculator.java
+++ src/main/java/com/atguigu/Calculator.java
@@ -12,5 +12,11 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
+        }
+        return a / b;
+    }
 
 }
--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -10,6 +10,8 @@ public class Main {
         System.out.println("Add: " + calculator.add(a, b));
         System.out.println("Subtract: " + calculator.subtract(a, b));
         System.out.println("Multiply: " + calculator.multiply(a, b));
+        System.out.println("Divide: " + calculator.divide(a, b));
+
 
     }
 }
